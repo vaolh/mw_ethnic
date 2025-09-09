e48b1b1358cf60e838b8e496c913c206042b9385
Mati_Carrasco <102548696+matiascarrascoj@users.noreply.github.com>
2025-09-09 11:27:34 -0600

    Update deflators.R

--- data/deflators/code/deflators.R
+++ data/deflators/code/deflators.R
@@ -16,18 +16,11 @@ inpc_all$Fecha <- seq(as.Date("1970-01-01"), by = "month", length.out = nrow(inp
 inpc_all$Año_int <- as.integer(format(inpc_all$Fecha, "%Y"))
 inpc_all$Mes     <- as.integer(format(inpc_all$Fecha, "%m"))
 
-aug_ref <- inpc_all %>%
-  filter(Mes == 8) %>%
-  transmute(AñoRef = Año_int, August_INPC = INPC)
-
 inpc <- inpc_all %>%
   filter((Año_int >= 2015) & (Año_int != 2025))
 
 inpc <- inpc %>%
-  mutate(AñoRef = if_else(Mes == 12, Año_int + 1L, Año_int)) %>%
-  left_join(aug_ref, by = "AñoRef") %>%
-  mutate(Deflator = INPC / August_INPC) %>%
-  rename(Año = Año_int)
+  mutate(Deflator = INPC / INPC[which(inpc$Año_int == 2024 & inpc$Mes == 8)])
 
 
 
